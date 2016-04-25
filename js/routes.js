33dc1b26fc3a84d85bc82835248b56478d1d70bf
Dario Seminara <darios3@gmail.com>
2016-04-24 21:00:58 -0300

    hack to bypass caching

--- js/routes.js
+++ js/routes.js
@@ -1,29 +1,31 @@
 var trackermanAdmin = angular.module("TrackermanAdmin");
 
+var version = Math.floor(Math.random()*100000);
+
 trackermanAdmin.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/clients', {
-      templateUrl: 'templates/clients/list.html',
+      templateUrl: 'templates/clients/list.html?version=' + version,
       controller: 'ClientListController'
     })
     .when('/clients/:id', {
-      templateUrl: 'templates/clients/edit.html',
+      templateUrl: 'templates/clients/edit.html?version=' + version,
       controller: 'ClientEditController'
     })
     .when('/products', {
-      templateUrl: 'templates/products/list.html',
+      templateUrl: 'templates/products/list.html?version=' + version,
       controller: 'ProductListController'
     })
     .when('/products/:id', {
-      templateUrl: 'templates/products/edit.html',
+      templateUrl: 'templates/products/edit.html?version=' + version,
       controller: 'ProductEditController'
     })
     .when('/brands', {
-      templateUrl: 'templates/brands/list.html',
+      templateUrl: 'templates/brands/list.html?version=' + version,
       controller: 'BrandListController'
     })
     .when('/brands/:id', {
-      templateUrl: 'templates/brands/edit.html',
+      templateUrl: 'templates/brands/edit.html?version=' + version,
       controller: 'BrandEditController'
     })    
     .otherwise({redirectTo : '/clients'});
