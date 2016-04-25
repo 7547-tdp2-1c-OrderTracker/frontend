946fd914c962eba911f7b7105cf6fdd95444e9a8
Dario Seminara <darios3@gmail.com>
2016-04-24 21:06:37 -0300

    fixed entity creation

--- js/controllers.js
+++ js/controllers.js
@@ -36,7 +36,13 @@ var editController = function(pluralName, options) {
   beforeSave = options.beforeSave || function(x){return x; };
 
   return ["$scope", "$http", "$resource", "$routeParams", "$window", function($scope, $http, $resource, $routeParams, $window) {
-    var Entity = $resource(baseUrl + "/v1/"+pluralName+"/:id", {id:"@id"}, {save: {method: 'PUT'}});
+    var Entity;
+
+    if ($routeParams.id === "new") {
+      Entity = $resource(baseUrl + "/v1/"+pluralName);
+    } else {
+      Entity = $resource(baseUrl + "/v1/"+pluralName+"/:id", {id:"@id"}, {save: {method: 'PUT'}});
+    }
 
     var processRelation = function(fieldName, relData) {
       $http.get(baseUrl + "/v1/" + relData.pluralName + "?limit=999999").then(function(response) {
@@ -57,9 +63,13 @@ var editController = function(pluralName, options) {
       }
     }
 
-    Entity.get({id: $routeParams.id}).$promise.then(function(entity) {
-      $scope.entity = entity;
-    });
+    if ($routeParams.id !== "new") {
+      Entity.get({id: $routeParams.id}).$promise.then(function(entity) {
+        $scope.entity = entity;
+      });
+    } else {
+      $scope.entity = new Entity();
+    }
 
     $scope.save = function() {
       beforeSave($scope.entity).$save(function(entity) {
